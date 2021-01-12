88a87cbe3b98549e83fa4128441efa37be725d89
Jorge Luis <jorgeluisordonez1995@gmail.com>
2021-01-11 23:39:48 -0500

    comit inicial

---
+++ src/components/GifGrid.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
+/* import { getGifs } from '../helpers/getGifs';*/
+import { GifGridItem } from './GifGridItem'; 
+
+export const GifGrid = ({ category }) => {
+
+    const { data:images,loading } = useFetchGifs(category);
+    /*  const [images, setImages] = useState([]);
+     useEffect(() => {
+         getGifs(category)
+             //.then(imgs => setImages(imgs))
+             .then(setImages)
+             .catch(e => console.log);
+     }, [category]); */
+    return (
+        <>
+            <h3>{category}</h3>
+            {loading && <p>Cargando...</p>}
+            <div className='card-grid'>
+                {
+                       images.map(img => (
+                           <GifGridItem
+                               key={img.id}
+                               {...img}
+                           />
+                       ))
+                }
+
+            </div>
+        </>
+    )
+}
